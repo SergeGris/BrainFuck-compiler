cade3545decd9cf5171deccd8f8fe3a1b236a7f7
smplcdr <42321357+smplcdr@users.noreply.github.com>
2018-11-14 18:24:29 +0300

    Add files via upload

--- bfc.c
+++ bfc.c
@@ -96,8 +96,12 @@ int main(int argc, char **argv)
 {
     if (argc <= 2)
     {
-        printf("Usage: ./bfc [source filename] [output filename]\nCount of arguments can not be more than 2.\nIf you want to compile more than 2 BrainFuck source code, you have to ... FUCK! You seriously want to compile more than 1 file?! If yes - fuck you");
+        printf("Usage: ./bfc [source filename] [output filename]\n");
         exit(1);
+    }
+    if (argc > 3)
+    {
+        printf("\nCount of arguments can not be more than 2.\nIf you want to compile more than 1 BrainFuck source code, you have to ...\nFUCK!\nYou seriously want to compile more than 1 file?!\nIf yes - fuck you")
     }
     char *in_filename = argv[1];
     char *out_filename = argv[2];
@@ -108,26 +112,26 @@ int main(int argc, char **argv)
     const char *bfcompiler = "./brainfuck ";
     const char *asmdir = " ";
     const char *nasmflags = "nasm -f elf32 ";
-    const char *ldflags = " ld -m elf_i386 -s -o ";
+    const char *ldflags = "ld -m elf_i386 -s -o ";
     const char *objext = ".o ";
     
     char compile[1024];
     strcpy(compile, bfcompiler);
     strcat(compile, in_filename);
-    strcat(compile, asmdir);
+    strcat(compile, " ");
     strcat(compile, out_filename);
     printf("%s\n", compile);
     system(compile);
     char asm[1024];
     strcpy(asm, nasmflags);
-    strcat(asm, asmdir);
+    strcat(asm, " ");
     strcat(asm, out_filename);
     printf("%s\n", asm);
     system(asm);
     char link[1024];
     strcpy(link, ldflags);
     strcat(link, out_filename);
-    strcat(link, asmdir);
+    strcat(link, " ");
     strcat(link, out_filename);
     strcat(link, objext);
     printf("%s\n", link);
